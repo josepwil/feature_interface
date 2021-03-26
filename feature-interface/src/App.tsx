b32e791357bac186497bc7a0c7ec7e849080a6e3
Joseph Williams <josephwilliamsjs@gmail.com>
2021-03-26 12:28:56 -0400

    adding mock data and cleaning up react-app boilerplate

--- feature-interface/src/App.tsx
+++ feature-interface/src/App.tsx
@@ -3,22 +3,66 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // mock data
+  const features = [
+    {
+      name: 'Feature A',
+      cost: 0,
+      subFeatures: [
+        {
+          name: 'Sub-feature A-1',
+          cost: 20,
+          subFeatures: []
+        },
+        {
+          name: 'Sub-feature A-2',
+          cost: 0,
+          subFeatures: [
+            {
+              name: 'Sub-feature A-2-1',
+              cost: 50,
+              subFeatures: []
+            },
+            {
+              name: 'Sub-feature A-2-2',
+              cost: 25,
+              subFeatures: []
+            }
+          ]
+        },
+        {
+          name: 'Sub-feature A-3',
+          cost: 30,
+          subFeatures: []
+        }
+      ]
+    },
+    {
+      name: 'Feature B',
+      cost: 30,
+      subFeatures: []
+    },
+    {
+      name: 'Feature C',
+      cost: 40,
+      subFeatures: []
+    }
+  ]
+  
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <h3>Subscription Preferences</h3>
       </header>
+
+      <footer>
+        <h3>Total: $__ / mo</h3>
+        <button>Save</button>
+      </footer>
+      
     </div>
   );
 }
