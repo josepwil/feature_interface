b6d76d0fba4130493ceff3cd09c1938f771fbc00
Joseph Williams <josephwilliamsjs@gmail.com>
2021-03-26 15:46:34 -0400

    creating functions to handle CheckboxClicked and SubFeaturesListChange

--- feature-interface/src/FeaturesList.tsx
+++ feature-interface/src/FeaturesList.tsx
@@ -1,14 +1,36 @@
 import { IFeature, IProps } from './types'
+import Checkbox from './Checkbox'
 
-const FeaturesList: React.FC<IProps> = ({ features }) => {
+const FeaturesList: React.FC<IProps> = ({ features, selectedFeatures, onChange }) => {
+
+  const handleCheckBoxClicked = (featureName: string) => {
+    if(selectedFeatures[featureName]) {
+      delete selectedFeatures[featureName]
+    } else {
+      selectedFeatures[featureName] = {}
+    }
+    onChange(selectedFeatures)
+  }
+
+  const handleSubFeaturesListChange = (featureName: string, subSelections: any) => {
+    selectedFeatures[featureName] = subSelections;
+    onChange(selectedFeatures)
+  }
+
   return (
     <div>
       {features.map((feature: IFeature) => (
         <ul>
-          <p>{feature.name}</p>
-          {feature.subFeatures.length > 0 && 
+          <Checkbox 
+            label={feature.name}
+            selected={true}
+            onChange={() => handleCheckBoxClicked(feature.name)}
+          />
+          {(feature.subFeatures.length > 0 && selectedFeatures[feature.name]) &&
             <FeaturesList 
               features={feature.subFeatures}
+              selectedFeatures={selectedFeatures[feature.name]}
+              onChange={(subSelections: any) => handleSubFeaturesListChange(feature.name, subSelections)}
             />
           }
         </ul>
